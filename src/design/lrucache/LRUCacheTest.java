c3aeb6b0848a94701e4a252b64478b3f5d9b06f1
Navneet Singh <navneet.singh@paytm.com>
2016-10-03 15:14:12 +0530

    LRU Cache
    - implemened LRU cache

---
+++ src/design/lrucache/LRUCacheTest.java
@@ -0,0 +1,13 @@
+package design.lrucache;
+
+public class LRUCacheTest {
+
+  public static void main(String[] args) {
+    Cache cache = new LRUCache();
+    cache.put("k1", 1);
+    cache.put("k2", "Navneet");
+    cache.put("k3", new int[]{1,2,4});
+    System.out.println(cache.get("k3"));
+  }
+
+}
