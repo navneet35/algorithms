927bb12ef759c09a13177421545fb4f244440e0c
Navneet Singh <navneet.singh@paytm.com>
2016-10-20 15:00:40 +0530

    singleton design pattern

---
+++ src/designpatterns/singletonpattern/Singleton.java
@@ -0,0 +1,16 @@
+package designpatterns.singletonpattern;
+
+public class Singleton {
+  private Singleton(){
+    System.out.println("In singleton constructor.");
+  }
+  
+  private static class SingletonHelper{
+    private static final Singleton singletonInstance = new Singleton();
+  }
+  
+  public static Singleton getInstance(){
+    return SingletonHelper.singletonInstance;
+  }
+
+}
---
+++ src/designpatterns/singletonpattern/SingletonTest.java
@@ -0,0 +1,9 @@
+package designpatterns.singletonpattern;
+
+public class SingletonTest {
+
+  public static void main(String[] args) {
+    Singleton singletonInstance = Singleton.getInstance();
+  }
+
+}
